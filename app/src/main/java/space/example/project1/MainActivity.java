23ef4b920211ec7159317322bec2aea64445c2dc
Nazar <nazikulishak@gmai;.com>
2021-05-26 22:21:33 +0300

    save commit

--- app/src/main/java/space/example/project1/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package space.example.project1;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-}
--- app/src/main/java/space/example/project1/MainActivity.kt
+++ app/src/main/java/space/example/project1/MainActivity.kt
@@ -5,57 +5,48 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import java.util.*
 
+class MainActivity : Activity() {
+    var imSemafor: ImageView? = null
+    var counter: Int = 0
+    var timer: Timer? = null
+    var is_run = false
+    var imageArray: IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_yellow, R.drawable.semafor_green)
 
-class MainViewModel(mainActivity: MainActivity) : ViewModel() {
-    class MainActivity : Activity() {
-        var imSemafor: ImageView? = null
-        var counter: Int = 0
-        var timer: Timer? = null
-        var is_run = false
-        var imageArray: IntArray = intArrayOf(R.drawable.semafor_red, R.drawable.semafor_yellow, R.drawable.semafor_green)
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            imSemafor = findViewById(R.id.imSemafor)
-            val model: MainViewModel = MyViewModel(this).get(MainViewModel::class.java)
-            val data: LiveData<String>?
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imSemafor = findViewById(R.id.imSemafor)
+    }
 
-        fun onClickStart(view: View) {
-            imSemafor?.setImageResource(R.drawable.semafor_grey)
-            view as ImageButton
+    fun onClickStart(view: View) {
+        view as ImageButton
+        if (!is_run) {
+
+            startStop()
             view.setImageResource(R.drawable.button_stop)
-            if (!is_run) {
-                timer = Timer()
-                startStop()
-                view.setImageResource(R.drawable.button_stop)
-                is_run = true
-            } else {
-                imSemafor?.setImageResource(R.drawable.semafor_grey)
-                view.setImageResource(R.drawable.button_start)
-                timer?.cancel()
-                view.setImageResource(R.drawable.button_start)
-                is_run = false
-                counter = 0
-            }
-        }
-
-        fun startStop() {
-            timer = Timer()
-            timer?.schedule(object : TimerTask() {
-                override fun run() {
-                    runOnUiThread {
-                        imSemafor?.setImageResource(imageArray[counter])
-                        counter++
-                        if (counter == 3) counter = 0
-                    }
-                }
-            }, 0, 1000)
+            is_run = true
+        } else {
+            imSemafor?.setImageResource(R.drawable.semafor_grey)
+            view.setImageResource(R.drawable.button_start)
+            timer?.cancel()
+            is_run = false
+            counter = 0
         }
     }
-}
 
+    fun startStop() {
+        timer = Timer()
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    imSemafor?.setImageResource(imageArray[counter])
+                    counter++
+                    if (counter == 3) counter = 0
+                }
+            }
+
+        }, 0, 1000)
+    }
+}
